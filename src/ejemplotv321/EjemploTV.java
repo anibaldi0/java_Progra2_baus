67fe33b12e3624b6e56834a1bb6e4ac655d47ffc
nibalink-pc <nibalink-pc@host.docker.internal>
2024-10-06 09:09:32 -0300

    inicializando_guia321

---
+++ src/ejemplotv321/EjemploTV.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplotv321;
+
+/**
+ *
+ * @author samue
+ */
+public class EjemploTV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        subirVolumen();  // Subir el volumen de 0 a 5
+        bajarVolumen();  // Bajar el volumen de 5 a 0
+    }
+    
+}
